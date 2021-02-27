33f7c264d1f9452eb7451afbb95287c1f2d835ba
cutiedaisy <otoorashid@yahoo.com>
2021-02-27 17:38:13 +0000

    Add project files.

---
+++ HelloWorld/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Controllers
+{
+    //[ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public WeatherForecastController()
+        {
+
+        }
+
+
+        [HttpGet("answer")]
+        public int GetAnswer()
+        {
+            var answer = 6 + 8;
+            return answer;
+        }
+
+        [HttpGet("age")]
+        public int GetAge()
+        {
+            var age = 18;
+            return age;
+        }
+
+        [HttpGet("name")]
+        public string GetName()
+        {
+            var name = "yaw";
+            return name;
+        }
+    }
+
+
+}
---
+++ HelloWorld/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Person
+    {
+        public Person() { }
+
+        public Person(int age, string name, string gender)
+        {
+            Age = age;
+            Name = name;
+            Gender = gender;
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+}
